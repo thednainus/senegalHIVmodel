f2de3fa5856f2ef0158e17a623cae51c40ecf4a4
Fabricia Nascimento <thednainus@yahoo.com>
2018-02-27 20:25:46 +0000

    Senegal HIV model

--- R/miscellaneous.R
+++ R/miscellaneous.R
@@ -40,3 +40,64 @@ setup.model.equations <- function(demes, nondemes = NULL, rcpp = FALSE)
        nonDemeDynamics = ndd, m = m, mm = mm ,
        demes = demes, nondemes = nondemes)
 }
+
+
+#' Organize HIV metadata
+#'
+#' This function aims to organize the HIV metadata in a format that will be more
+#' useful for the analysis of HIV transmission in Senegal using coalescent model.
+#' It mainly gets information on different dataframes format and organize them so as to
+#' create a matrix for the states (demes) on the phylogenetic tree
+#'
+#' @param metadata_CGR data.frame object that contains metadata for the CGR sequences.
+#'    These sequeces will receive the state "src", a deme in our mathematical model.
+#' @param metadata_SN data.frame object that contain all metadata for the Senegal (SN)
+#'    sequences. Sequences will receive the states: "gpf", "gpm", or "msm".
+#' @param to_remove data.frame object that contain all metadata that must be removed
+#'    from analysis because there are missing information
+#' @param tree phylogenetic tree in the class phylo in which the tips correspond to
+#'    all sequences with metadata
+#'
+#' @return a data.frame object containing 2 columns. The first column reprsent the
+#'    the sequences name (as in the phylogenetic tree), and second column represent
+#'    the state of the sequences. In our case, state can be any of the demes (gpf, gpm,
+#'    msm, and src).
+#'
+#' @export
+#'
+#' @examples
+#' tree.all <- read.tree("data/bindTree_CGR_GTR+Gp12+3_droppedTip.tre")
+#' all.data.cgr <- read.csv("data/HIV_subtypes_summary_CGR.csv")
+#' all.data.SN <- read.csv("data/HIV_subtypes_summary_SENEGAL_noDups.csv")
+#' to.remove <- read.csv("data/HIV_subtypes_SENEGAL_noDups_ToDrop_fromAnalysis.csv")
+#'
+#' all_data <- organize_metadata(all.data.cgr, all.data.SN, to.remove, tree.all)
+organize_metadata <- function(metadata_CGR, metadata_SN, to_remove, tree){
+
+  # Organize metadata for CGR sequences
+  metadata_CGR["tip.name"] <- paste(metadata_CGR$Accession_number, "CGR", sep='_')
+  metadata_CGR.2 <- metadata_CGR[c("tip.name", "Risk_group")]
+  metadata_CGR.2["Sex"] <- NA
+  metadata_CGR.2["States"] <- "src"
+  metadata_CGR.3 <- metadata_CGR.2[c("tip.name", "States")]
+
+  # Organize metadata for the Senegal sequences
+  # It creates a dataframe that will not have metadata with missing information
+  metadata_SN.2 <- metadata_SN[ ! metadata_SN$Accession_number %in% to_remove$Accession_number, ]
+  metadata_SN.2["tip.name"] <- paste(metadata_SN.2$Accession_number, metadata_SN.2$Subtype, "SN", metadata_SN.2$Year, sep='.')
+  metadata_SN.3 <- metadata_SN.2[c("tip.name", "Risk_group", "Sex")]
+  metadata_SN.3["States"] <- tolower(paste(metadata_SN.3$Risk_group, metadata_SN.3$Sex, sep = ""))
+  metadata_SN.3$States[metadata_SN.3$Risk_group == "MSM"] <- "msm"
+  metadata_SN.4 <- metadata_SN.3[c("tip.name", "States")]
+
+  # bind together both dataframes (the one containing metadata on Senegal samples,
+  # and the other containing metadata on CGR sequences)
+  all_data <- rbind(metadata_CGR.3, metadata_SN.4)
+  # Match the information on the tip of the tree with the dataframe containing information
+  # on all data
+  all_data.2 <- all_data[match(tree$tip.label, all_data$tip.name),]
+  all_data.2$States <- as.factor(all_data.2$States)
+
+  return(all_data.2)
+
+}
--- analysis/scripts/load_data.R
+++ analysis/scripts/load_data.R
@@ -3,58 +3,39 @@
 # were missing
 tree.all <- read.tree("data/bindTree_CGR_GTR+Gp12+3_droppedTip.tre")
 
+# Reads metadata that contain info for the CGR (close global reference) sequences
+# CGRs are referred in the mathematical model as src (source) data
+# These are HIV sequences that are from other countries and not from Senegal
 all.data.cgr <- read.csv("data/HIV_subtypes_summary_CGR.csv")
-all.data.cgr["tip.name"] <- paste(all.data.cgr$Accession_number, "CGR", sep='_')
-all.data.cgr2 <- all.data.cgr[c("tip.name", "Risk_group")]
-all.data.cgr2["Sex"] <- NA
-all.data.cgr2["States"] <- "src"
-all.data.cgr3 <- all.data.cgr2[c("tip.name", "States")]
 
-
-
+# Reads all metadata for HIV sequences from Senegal
 all.data.SN <- read.csv("data/HIV_subtypes_summary_SENEGAL_noDups.csv")
-#remove tips that are not in the tree
-#to.remove is a dataframe that contains info on sequences that should be dropped from analysis
+
+# Read all metadata that needs to be removed because information is missing
+# These are information related to the deme, for example, some sequences we don't
+# have information whether it is a male or female from the general population
 to.remove <- read.csv("data/HIV_subtypes_SENEGAL_noDups_ToDrop_fromAnalysis.csv")
-all.data.SN.2 <- all.data.SN[ ! all.data.SN$Accession_number %in% to.remove$Accession_number, ]
 
-
-all.data.SN.2["tip.name"] <- paste(all.data.SN.2$Accession_number, all.data.SN.2$Subtype, "SN", all.data.SN.2$Year, sep='.')
-all.data.SN.3 <- all.data.SN.2[c("tip.name", "Risk_group", "Sex")]
-
-
-all.data.SN.3["States"] <- tolower(paste(all.data.SN.3$Risk_group, all.data.SN.3$Sex, sep = ""))
-all.data.SN.3$States[all.data.SN.3$Risk_group == "MSM"] <- "msm"
-all.data.SN.4 <- all.data.SN.3[c("tip.name", "States")]
-
-
-# bind together both dataframes (the one containing info on Senegal samples, and
-# the other containing info on CGR sequences)
-all.data <- rbind(all.data.cgr3, all.data.SN.4)
-all.data.2 <- all.data[match(tree.all$tip.label, all.data$tip.name),]
-#all.data.2 <- all.data[match(tr3$tip.label, all.data$tip.name),]
-
-
-all.data.2$States <- as.factor(all.data.2$States)
+# organize metadata in 2 columns.
+# the first column is the sequences name as in the phylogenetic tree
+# the second colum is the state (gpf, gpm, msm, or src) of each sequences
+all_data <- organize_metadata(all.data.cgr, all.data.SN, to.remove, tree.all)
 
 #create matrix to receive the information on states for each tip of the tree
 gpm <- gpf <- msm <- src <- rep(0, length(tree.all$tip.label))
-#gpm <- gpf <- msm <- src <- rep(0, length(tr3$tip.label))
 
-gpm[all.data.2$States == "gpm"] <- 1
-gpf[all.data.2$States == "gpf"] <- 1
-msm[all.data.2$States == "msm"] <- 1
-src[all.data.2$States == "src"] <- 1
+gpm[all_data$States == "gpm"] <- 1
+gpf[all_data$States == "gpf"] <- 1
+msm[all_data$States == "msm"] <- 1
+src[all_data$States == "src"] <- 1
 
 sampleStates <- cbind(gpm, gpf, msm, src)
-rownames(sampleStates) <- all.data.2$tip.name
+rownames(sampleStates) <- all_data$tip.name
 
-
-#DatedTree function (phylo, sampleTimes, sampleStates = NULL, sampleStatesAnnotations = NULL,
-#          tol = 1e-06, minEdgeLength = 0)
-
+#read estimated times (in calendar units) for each sequence in the phylogenetic tree
 times <- readRDS("data/bindTree_CGR_GTR+Gp12+3_droppedTip_sts.RDS")
-#times.ordered <- times[match(tree.all$tip.label, names(times))]
 
-
+# create an object of DatedTree [phydynR package]
+# This is the tree that should be used in the calculation of the likelihood
+# to estimate parameter values
 dated.tree <- DatedTree(phylo = tree.all, sampleTimes = times, sampleStates = sampleStates, minEdgeLength = 2/52, tol = .1)
